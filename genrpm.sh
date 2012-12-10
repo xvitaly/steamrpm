fc047288340e57786a2f7c48b361d303036c9a1e
V1TSK <vitaly@easycoding.org>
2012-12-10 16:56:58 +0600

    Small fixes.

--- genrpm.sh
+++ genrpm.sh
@@ -13,20 +13,20 @@ wget http://media.steampowered.com/client/installer/steam.deb
 if [ -f "$TMPATH/steam.deb" ]; then
   echo Unpacking Ubuntu package...
   ar vx steam.deb
-
-  echo Checking rpmbuild working directory...
-  if [ ! -d ~/rpmbuild/SOURCES/ ]; then
-    echo Creating SOURCES in rpmbuild working directory...
-    mkdir -p ~/rpmbuild/SOURCES/
-  fi
-
+  
   if [ -f "$TMPATH/data.tar.gz" ]; then
+    echo Checking rpmbuild working directory...
+    if [ ! -d ~/rpmbuild/SOURCES/ ]; then
+      echo Creating SOURCES in rpmbuild working directory...
+      mkdir -p ~/rpmbuild/SOURCES/
+    fi
+    
     echo Copying archive with binaries to rpmbuild working directory...
     cp -f data.tar.gz ~/rpmbuild/SOURCES/steam.tar.gz
     
     echo Downloading SPEC file...
     wget https://github.com/xvitaly/steamrpm/raw/master/steam.spec
-
+    
     if [ -f "$TMPATH/steam.spec" ]; then
       echo Building package...
       rpmbuild -ba steam.spec
