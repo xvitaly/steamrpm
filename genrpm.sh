eca4fd012696f41cb25d173dce26bf20b8beb765
V1TSK <vitaly@easycoding.org>
2013-12-04 18:12:53 +0600

    * The script is now silent.

--- genrpm.sh
+++ genrpm.sh
@@ -5,11 +5,11 @@ TMPATH=`mktemp -d`
 cd $TMPATH
 
 echo Downloading Ubuntu package...
-wget http://media.steampowered.com/client/installer/steam.deb -O $TMPATH/steam.deb
+wget http://media.steampowered.com/client/installer/steam.deb -O $TMPATH/steam.deb 2> /dev/null
 
 if [ -f "$TMPATH/steam.deb" ]; then
   echo Unpacking Ubuntu package...
-  ar vx steam.deb
+  ar vx steam.deb > /dev/null 2> /dev/null
   
   if [ -f "$TMPATH/data.tar.gz" ]; then
     echo Checking rpmbuild working directory...
@@ -22,12 +22,12 @@ if [ -f "$TMPATH/steam.deb" ]; then
     cp -f data.tar.gz ~/rpmbuild/SOURCES/steam.tar.gz
     
     echo Downloading SPEC file...
-    wget https://github.com/xvitaly/steamrpm/raw/master/steam.spec
+    wget https://github.com/xvitaly/steamrpm/raw/master/steam.spec -O $TMPATH/steam.spec 2> /dev/null
     
     if [ -f "$TMPATH/steam.spec" ]; then
       
       if [ -f "$TMPATH/data.tar.gz" ]; then
-	tar -zxf control.tar.gz -C $TMPATH
+	tar -zxf control.tar.gz -C $TMPATH > /dev/null 2> /dev/null
 	if [ -f "$TMPATH/control" ]; then
 	  echo Updating version info from DEB package...
 	  sed -i "s/Version: .*/Version: `awk '/Version:/ {print $2}' control`/g" steam.spec
@@ -35,7 +35,7 @@ if [ -f "$TMPATH/steam.deb" ]; then
       fi
       
       echo Building package...
-      rpmbuild -bb --clean --buildroot ~/rpmbuild/BUILDROOT/steam-root steam.spec
+      rpmbuild -bb --clean --buildroot ~/rpmbuild/BUILDROOT/steam-root steam.spec > /dev/null 2> /dev/null
     fi
   fi
 fi
